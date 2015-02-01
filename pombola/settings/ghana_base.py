706eedcd357210627c5d58cce45e468814c14f42
Ian <idesouza@hutspace.net>
2015-02-01 18:29:25 -0500

    actual initial merge running on clean db

---
+++ pombola/settings/ghana_base.py
@@ -0,0 +1,28 @@
+COUNTRY_APP = 'ghana'
+
+OPTIONAL_APPS = [
+    'pombola.hansard',
+    'pombola.projects',
+    'pombola.place_data',
+    'pombola.votematch',
+]
+
+TWITTER_USERNAME = ''
+TWITTER_WIDGET_ID = ''
+
+BLOG_RSS_FEED = ''
+
+MAP_BOUNDING_BOX_NORTH = 11.17
+MAP_BOUNDING_BOX_EAST = 1.19
+MAP_BOUNDING_BOX_SOUTH = 4.73
+MAP_BOUNDING_BOX_WEST = -3.25
+
+MAPIT_COUNTRY = 'GH'
+
+COUNTRY_CSS = {
+    
+}
+
+COUNTRY_JS = {
+    
+}
