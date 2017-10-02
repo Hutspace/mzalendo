a8cc4527d737fa7cfea0c2b0b47be92b289e1fa5
Enock Kwesi Addey <enock.addey@gmail.com>
2017-10-02 14:54:04 +0000

    Uncommented 'pombola.wordcloud',

--- pombola/settings/ghana_base.py
+++ pombola/settings/ghana_base.py
@@ -15,7 +15,7 @@ OPTIONAL_APPS = (
     'pombola.projects',
     'pombola.place_data',
     'pombola.votematch',
-    # 'pombola.wordcloud',
+    'pombola.wordcloud',
 
     # Pombola has now dropped registration, so leave these out:
     # 'registration_defaults',
